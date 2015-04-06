a4f317f3ca10a51d4739775938abfeb6d32b612f
HelderCastrillon <helder_947@hotmail.com>
2015-04-05 20:05:14 -0500

    Ajutes de formulario

--- modules/Mission/missionController.js
+++ modules/Mission/missionController.js
@@ -1,24 +1,38 @@
 appConfigProjectMSF.controller('missionController', ["$scope",'$filter',"commonvariable","Mission", function($scope, $filter,commonvariable,Mission) {
 	var $translate = $filter('translate');
-
+	$scope.showfields=false;
 	console.log(commonvariable.OrganisationUnit);
-
 	$scope.missionsave=function(){
 		Mission.POST({uid:commonvariable.OrganisationUnit.id,name:$scope.mdname,openingDate:$scope.mdopendate});
 		console.log($scope.mdname);
-
-		$scope.showfields=function(){
-			$scope.show=true;
-		}
-
-		$scope.$watch(
-			function($scope) {
-				$scope.orgunitname=commonvariable.OrganisationUnit.name;
-			});
-
-
-
 	};
+	$scope.showForm=function(){
+		$scope.showfields=true;
+	};
+	$scope.hideForm=function(){
+		$scope.projectName="";
+		$scope.today();
+		$scope.showfields=false;
+	};
+	
+    // Date datepicker
+	  $scope.today = function() {
+	    datetoday = new Date();
+	    $scope.projectDate=datetoday.getFullYear()+"-"+((datetoday.getMonth()+1)<=9?"0"+(datetoday.getMonth()+1):(datetoday.getMonth()+1))+"-"+(datetoday.getDate()<=9?"0"+datetoday.getDate():datetoday.getDate());
+	  };
+	  $scope.today();
+
+	  $scope.clear = function () {
+	    $scope.projectDate = null;
+	  };
+
+	   $scope.open = function($event) {
+		    $event.preventDefault();
+		    $event.stopPropagation();
+		    $scope.opened = true;
+	  };
+/////////////////////////////////////////
+	
 }]);
 
 
