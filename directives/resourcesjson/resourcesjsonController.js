850f0e23b1b6773b91456fd22c7e566b83a6b767
HelderCastrillon <helder_947@hotmail.com>
2015-06-24 23:24:37 -0500

    showing data of json file

--- directives/resourcesjson/resourcesjsonController.js
+++ directives/resourcesjson/resourcesjsonController.js
@@ -8,9 +8,25 @@ Dhis2Api.directive('d2Resourcejson', function(){
 	}
 	}); 
 Dhis2Api.controller("d2ResourcejsonController", ['$scope',"commonvariable","loadjsonresource", function ($scope,commonvariable,loadjsonresource) {
-	loadjsonresource.get($scope.id)
-    .then(function(response){
-        console.log(response.data);
-    });
+	
+    $scope.loadjson=function(){
+        loadjsonresource.get($scope.id)
+        .then(function(response){
+                $scope.sections=response.data.vaccinationDataset[0];
+        });
+    }
+
+    $scope.toType = function(obj) {
+        return ({}).toString.call(obj).match(/\s([a-z|A-Z]+)/)[1].toLowerCase()
+    }
+
+    $scope.readjson=function(datavalue){
+         angular.forEach(datavalue, function(value, key){
+                var typeobject=$scope.toType(value);
+                if(typeobject=="array"||typeobject=="object")
+                    $scope.readjson(value);
+            });
+    }
+
     }]);
 
