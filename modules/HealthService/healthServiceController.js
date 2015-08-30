cadd241ac5759cb74e2ea60c1ab69ef8f4d27078
josemp10 <josemp10@gmail.com>
2015-08-30 13:01:41 +0200

    Health Service Layout

--- modules/HealthService/healthServiceController.js
+++ modules/HealthService/healthServiceController.js
@@ -1,5 +1,41 @@
 appConfigProjectMSF.controller('healthServiceController', ["$scope",'$filter',"commonvariable", function($scope, $filter,commonvariable) {
 	var $translate = $filter('translate');
+	
+	//set message variable
+	$scope.closeAlertMessage = function(index) {
+       $scope.messages.splice(index, 1);
+  	};
+	
+	$scope.messages=[];
+	
+	
+	$scope.showfields=false;
+	console.log(commonvariable.OrganisationUnit);
+	
+	
+	$scope.showForm=function(frm){
+		
+		if(frm==1){
+			$scope.frmSite=true;
+		}
+		else{
+			$scope.frmSite=false;
+		}
+
+		
+	//	$scope.showfields=true;
+	};
+
+	$scope.$watch(
+			function($scope) {
+				if(commonvariable.OrganisationUnit!=undefined){
+					$scope.healthservicename=commonvariable.OrganisationUnit.name;
+			}
+			});
+
+	
+	
+	
 }]);
 
 
