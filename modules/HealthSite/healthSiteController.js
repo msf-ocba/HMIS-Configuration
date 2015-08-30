6535246522fc3016ec30c9869d258b3983d87cca
josemp10 <josemp10@gmail.com>
2015-08-30 11:21:51 +0200

    Health site layout

--- modules/HealthSite/healthSiteController.js
+++ modules/HealthSite/healthSiteController.js
@@ -1,5 +1,40 @@
 appConfigProjectMSF.controller('healthSiteController', ["$scope",'$filter',"commonvariable", function($scope, $filter,commonvariable) {
 	var $translate = $filter('translate');
+	
+	//set message variable
+	$scope.closeAlertMessage = function(index) {
+       $scope.messages.splice(index, 1);
+  	};
+	
+	$scope.messages=[];
+	
+	
+	$scope.showfields=false;
+	console.log(commonvariable.OrganisationUnit);
+	
+	
+	$scope.showForm=function(frm){
+		
+		if(frm==1){
+			$scope.frmSite=true;
+		}
+		else{
+			$scope.frmSite=false;
+		}
+
+		
+	//	$scope.showfields=true;
+	};
+	
+	$scope.$watch(
+			function($scope) {
+				if(commonvariable.OrganisationUnit!=undefined){
+					$scope.healthsitename=commonvariable.OrganisationUnit.name;
+					$scope.healthsitecreated=commonvariable.OrganisationUnit.created;
+			}
+			});
+
+	
 }]);
 
 
