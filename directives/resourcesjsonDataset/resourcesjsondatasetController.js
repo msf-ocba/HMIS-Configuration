eee280d9b1f731ebdf81c69b712556437adad932
HelderCastrillon <heldersoft@gmail.com>
2015-10-01 21:28:15 -0500

    DataSet Selected in healthService -part1

--- directives/resourcesjsonDataset/resourcesjsondatasetController.js
+++ directives/resourcesjsonDataset/resourcesjsondatasetController.js
@@ -8,86 +8,54 @@ Dhis2Api.directive('d2Resourcejsondataset', function(){
 	}
 	}); 
 Dhis2Api.controller("d2ResourcejsondatasetController", ['$scope', '$interval', "commonvariable", "loadjsonresource", "DataElements", function ($scope, $interval, commonvariable, loadjsonresource, DataElements) {
-	$scope.style=[];
-
+    $scope.style=[];
+    $scope.datasetcodeSelected = [];
 	
-	var stop;
-	$scope.prevOu = undefined;
+    var stop;
+    $scope.prevOu = undefined;
 
-	$scope.$watch(function () {
-	    if (commonvariable.OrganisationUnit && commonvariable.OrganisationUnit.id != $scope.prevOu) {
+    $scope.$watch(function () {
+        if (commonvariable.OrganisationUnit && commonvariable.OrganisationUnit.id != $scope.prevOu) {
 
-	        commonvariable.DataElementSelected = [];
-	        $scope.style = [];
+            commonvariable.DataElementSelected = [];
+            $scope.style = [];
  
-	        $scope.prevOu = commonvariable.OrganisationUnit.id;
-	    }
-	});
+            $scope.prevOu = commonvariable.OrganisationUnit.id;
+        }
+    });
 
 
 
 
 
-    $scope.loadjson=function(callBack){
+    $scope.loadlevel=function(callBack){
         $scope.GroupDE=[];
        loadjsonresource.get($scope.id)
         .then(function(response){
             $scope.services = response.data.datasetByService[0].service;
-                //get code of data element
-            angular.forEach($scope.services, function (vvalue, vkey) {
-                    angular.forEach(vvalue.levels, function (lvalue, lkey) {
-                        angular.forEach(lvalue.periods, function (pvalue, pkey) {
-                            angular.forEach(pvalue.datasets, function (dvalue, dkey) {
-
-                            });
-                        });
-                        
-
-                    });
-
+              angular.forEach($scope.services, function (svalue, skey) {
+                if (svalue.name == commonvariable.OrganisationUnit.name) 
+                    $scope.levels = svalue.levels;
                  });
-
-                if($scope.DEListCode){
-                    callBack();
-                }
-        
-
-        });
-
-        
+         });
     }
 
-    $scope.loadjson();
+    $scope.loadlevel();
 
-    $scope.selectgroup=function(key,skey){
-        ///add dataelement selected of all group
-         angular.forEach($scope.sections[skey].dataElementGroup[key].dataElements, function (dvalue, dkey) {
-             commonvariable.DataElementSelected.push({ id: $scope.ListnameDataelement[dvalue.code].id });
-         });
+    $scope.selectdataset= function(dcode) {
+        
+        var index = $scope.datasetcodeSelected.indexOf(dcode);
+        if (index > -1) {
+            $scope.datasetcodeSelected.splice(index, 1);
+            $scope.style[dcode] = '';
+        }
+        else {
+            $scope.style[dcode] = 'success';
+            $scope.datasetcodeSelected.push(dcode);
+        }
 
-         //config style for list of DataElements
-        if($scope.style[skey]==undefined)
-            $scope.style[skey]=[];
-        if($scope.style[skey][key]=='' || $scope.style[skey][key]==undefined)
-            $scope.style[skey][key]='success';
-        else
-            $scope.style[skey][key]='';
-
+        console.log($scope.datasetcodeSelected);
     };
 
-    $scope.selectgroupforEdit = function (key, skey) {
-        ///add dataelement selected of all group
-        angular.forEach($scope.sections[skey].dataElementGroup[key].dataElements, function (dvalue, dkey) {
-            commonvariable.DataElementSelected.push({ id: $scope.ListnameDataelement[dvalue.code].id });
-        });
-
-        //config style for list of DataElements
-        if ($scope.style[skey] == undefined)
-            $scope.style[skey] = [];
-        if ($scope.style[skey][key] == '' || $scope.style[skey][key] == undefined)
-            $scope.style[skey][key] = 'success';       
-
-    };
-
-    }]);
+   }]);
 
