301da04a7db470f7a0bf06bf87c262606cbd465b
HelderCastrillon <heldersoft@gmail.com>
2016-01-22 13:23:10 -0500

    Validation empty field in forms

--- modules/HealthSite/healthSiteController.js
+++ modules/HealthSite/healthSiteController.js
@@ -1,4 +1,4 @@
-appConfigProjectMSF.controller('healthSiteController', ["$scope", '$filter', "commonvariable", "OrgUnit", "OrgUnitGroupsOrgUnit", "loadjsonresource", "OrgUnitGroupByOrgUnit", "FilterResource", "$modal", "DataSetsOrgUnit", "GetMission", function ($scope, $filter, commonvariable, OrgUnit, OrgUnitGroupsOrgUnit, loadjsonresource, OrgUnitGroupByOrgUnit, FilterResource,$modal, DataSetsOrgUnit, GetMission) {
+appConfigProjectMSF.controller('healthSiteController', ["$scope", '$filter', "commonvariable", "OrgUnit", "OrgUnitGroupsOrgUnit", "loadjsonresource", "OrgUnitGroupByOrgUnit", "FilterResource", "$modal", "DataSetsOrgUnit", "GetMission", "validatorService", function ($scope, $filter, commonvariable, OrgUnit, OrgUnitGroupsOrgUnit, loadjsonresource, OrgUnitGroupByOrgUnit, FilterResource, $modal, DataSetsOrgUnit, GetMission, validatorService) {
 	var $translate = $filter('translate');
 	
 	//set message variable
@@ -30,124 +30,151 @@ appConfigProjectMSF.controller('healthSiteController', ["$scope", '$filter', "co
 					
 			healthServiceSuffix = getServiceSuffix(response.data.healthserviceSuffix).suffix;
 			
-			healthServiceCode=commonvariable.OrganisationUnit.code + "_" + healthServiceSuffix;
-						
-			if (commonvariable.OrganisationUnit.children.length>0)
-				healthServiceCode = healthServiceCode +"_" + (commonvariable.OrganisationUnit.children.length + 1);
-
-			var newOu={//payload
-					name:healthServiceName,				
+			
+			var prenewOu={//payload
+			        name: commonvariable.orgUnitGroupSet.BtFXTpKRl6n.name,
 					level:(commonvariable.OrganisationUnit.level+1),
-					code:healthServiceCode,
+					code: healthServiceSuffix,
 		            shortName:healthServiceName,
 		           	openingDate:$filter('date')($scope.healthServiceDate, 'yyyy-MM-dd'),
 		           	parent: commonvariable.OrganisationUnitParentConf
 					};
+		    ///validate if object is ok.
+			validatorService.emptyValue(prenewOu).then(function (result) {
+			    if (result == false) {
 
-			OrgUnit.POST({},newOu)
-			.$promise.then(function(data){
-	    		  console.log(data);
-	    		  if(data.response.status=="SUCCESS"){
-	    		  	  commonvariable.RefreshTreeOU=true;
-					  newOu.id=data.response.lastImported;
-					  commonvariable.NewOrganisationUnit=newOu;		
-					  
-					  if (commonvariable.orgUnitGroupSet.BtFXTpKRl6n!=undefined)
-						  OrgUnitGroupsOrgUnit.POST({uidgroup:commonvariable.orgUnitGroupSet.BtFXTpKRl6n.id, uidorgunit:newOu.id});
-					  				  					  
-					  console.log("Voy a crear la movida")
-					  
-					  console.log(commonvariable.orgUnitGroupSet.BtFXTpKRl6n.name)
-					  
-					  				  					  
-					  FilterResource.GET({resource:'dataSets', filter:'code:eq:'+"DS_INFR_3"}).$promise
-				  		.then(function(response){
-				  			
-				  			if (response.dataSets.length>0) {
-				  				
-				  				var dataSet = response.dataSets[0];
-				  				DataSetsOrgUnit.POST({uidorgunit:newOu.id, uiddataset:dataSet.id});
-				  			}
-				  							  			
-				  		});
-					
-					  if (commonvariable.orgUnitGroupSet.BtFXTpKRl6n.name == "Vaccination") { //Assocate Vacc datasets 
-					  
-						  GetMission.get({uid:newOu.id}).$promise.then(function(data){
-						  
-							  var nameMission=data.parent.parent.parent.name
-						  
-							  var nameVacDataSet = "Vaccination_"+nameMission
-						  
-							  FilterResource.GET({resource:'dataSets', filter:'name:eq:'+nameVacDataSet}).$promise
-					  			.then(function(response){
-					  			
-					  			if (response.dataSets.length>0) {
-					  				
-					  				var dataSet = response.dataSets[0];
-					  				DataSetsOrgUnit.POST({uidorgunit:newOu.id, uiddataset:dataSet.id});
-					  			}
-					  							  			
-					  			});						  
-						  
-						  });					  
-					  }
-					
-					  var codeServiceType = undefined;
-					  
-					  loadjsonresource.get("servicebyservicetype").then(function(response) {
-					  
-						  codeServiceType = getServiceType(response.data.servicesByServiceType);
-						  
-						  FilterResource.GET({resource:'organisationUnitGroups', filter:'code:eq:'+codeServiceType}).$promise
-					  		.then(function(response){
-					  			
-					  			if (response.organisationUnitGroups.length>0) {
-					  				
-					  				var orgUnitGroup = response.organisationUnitGroups[0];
-									OrgUnitGroupsOrgUnit.POST({uidgroup:orgUnitGroup.id, uidorgunit:newOu.id});
+			        healthServiceCode = commonvariable.OrganisationUnit.code + "_" + healthServiceSuffix;
 
-					  			}
-					  							  			
-					  		});
+			        if (commonvariable.OrganisationUnit.children.length > 0)
+			            healthServiceCode = healthServiceCode + "_" + (commonvariable.OrganisationUnit.children.length + 1);
 
-						  
-					  });
-					  
-					  
-					  OrgUnitGroupByOrgUnit.get({uid:commonvariable.OrganisationUnit.id}).$promise.then(function(response) {
-							
-						  listOrgUnitGroups = response.organisationUnitGroups;
-						  
-						  angular.forEach(listOrgUnitGroups, function (value, key){
-							  OrgUnitGroupsOrgUnit.POST({uidgroup:value.id, uidorgunit:newOu.id});
-						  });
-						  
-						  /*for (var i=0;i<listOrgUnitGroups.length;i++)
-							  OrgUnitGroupsOrgUnit.POST({uidgroup:listOrgUnitGroups[i].id,uidorgunit:newOu.id});*/
-						  
-					  });
+			        var newOu = {//payload
+			            name: healthServiceName,
+			            level: (commonvariable.OrganisationUnit.level + 1),
+			            code: healthServiceCode,
+			            shortName: healthServiceName,
+			            openingDate: $filter('date')($scope.healthServiceDate, 'yyyy-MM-dd'),
+			            parent: commonvariable.OrganisationUnitParentConf
+			        };
+
+			        OrgUnit.POST({}, newOu)
+                    .$promise.then(function (data) {
+                        console.log(data);
+                        if (data.response.status == "SUCCESS") {
+                            commonvariable.RefreshTreeOU = true;
+                            newOu.id = data.response.lastImported;
+                            commonvariable.NewOrganisationUnit = newOu;
+
+                            if (commonvariable.orgUnitGroupSet.BtFXTpKRl6n != undefined)
+                                OrgUnitGroupsOrgUnit.POST({ uidgroup: commonvariable.orgUnitGroupSet.BtFXTpKRl6n.id, uidorgunit: newOu.id });
+
+                            console.log("Voy a crear la movida")
+
+                            console.log(commonvariable.orgUnitGroupSet.BtFXTpKRl6n.name)
 
 
-					 //set message variable
-					$scope.messages.push({type:"success",
-					text:"Health service saved"});
+                            FilterResource.GET({ resource: 'dataSets', filter: 'code:eq:' + "DS_INFR_3" }).$promise
+                              .then(function (response) {
 
-					//clear txtbox
-					$scope.healthServiceName="";
-					
-					$scope.frmService = false;
+                                  if (response.dataSets.length > 0) {
 
-				}
-				else{
-					$scope.messages.push({type:"danger",
-					text:"Health service doesn't saved, review that the field name isn't empty"});
-				}
-	    	 });
+                                      var dataSet = response.dataSets[0];
+                                      DataSetsOrgUnit.POST({ uidorgunit: newOu.id, uiddataset: dataSet.id });
+                                  }
 
-			
+                              });
+
+                            if (commonvariable.orgUnitGroupSet.BtFXTpKRl6n.name == "Vaccination") { //Assocate Vacc datasets 
+
+                                GetMission.get({ uid: newOu.id }).$promise.then(function (data) {
+
+                                    var nameMission = data.parent.parent.parent.name
+
+                                    var nameVacDataSet = "Vaccination_" + nameMission
+
+                                    FilterResource.GET({ resource: 'dataSets', filter: 'name:eq:' + nameVacDataSet }).$promise
+                                      .then(function (response) {
+
+                                          if (response.dataSets.length > 0) {
+
+                                              var dataSet = response.dataSets[0];
+                                              DataSetsOrgUnit.POST({ uidorgunit: newOu.id, uiddataset: dataSet.id });
+                                          }
+
+                                      });
+
+                                });
+                            }
+
+                            var codeServiceType = undefined;
+
+                            loadjsonresource.get("servicebyservicetype").then(function (response) {
+
+                                codeServiceType = getServiceType(response.data.servicesByServiceType);
+
+                                FilterResource.GET({ resource: 'organisationUnitGroups', filter: 'code:eq:' + codeServiceType }).$promise
+                                  .then(function (response) {
+
+                                      if (response.organisationUnitGroups.length > 0) {
+
+                                          var orgUnitGroup = response.organisationUnitGroups[0];
+                                          OrgUnitGroupsOrgUnit.POST({ uidgroup: orgUnitGroup.id, uidorgunit: newOu.id });
+
+                                      }
+
+                                  });
+
+
+                            });
+
+
+                            OrgUnitGroupByOrgUnit.get({ uid: commonvariable.OrganisationUnit.id }).$promise.then(function (response) {
+
+                                listOrgUnitGroups = response.organisationUnitGroups;
+
+                                angular.forEach(listOrgUnitGroups, function (value, key) {
+                                    OrgUnitGroupsOrgUnit.POST({ uidgroup: value.id, uidorgunit: newOu.id });
+                                });
+
+                                /*for (var i=0;i<listOrgUnitGroups.length;i++)
+                                    OrgUnitGroupsOrgUnit.POST({uidgroup:listOrgUnitGroups[i].id,uidorgunit:newOu.id});*/
+
+                            });
+
+
+                            //set message variable
+                            $scope.messages.push({
+                                type: "success",
+                                text: "Health service saved"
+                            });
+
+                            //clear txtbox
+                            $scope.healthServiceName = "";
+
+                            $scope.frmService = false;
+
+                        }
+                        else {
+                            $scope.messages.push({
+                                type: "danger",
+                                text: "Health service doesn't saved, review that the field name isn't empty"
+                            });
+                        }
+                    });
+
+			    }
+			    else {
+			        $scope.messages.push({
+			            type: "warning",
+			            text: $translate("FORM_MSG_EMPTYFIELD")
+			        });
+			    }
+
+
+			});
+
 		});
-				
+
 	};
 	
 	getServiceSuffix = function(healthserviceSuffix) {
