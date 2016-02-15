7202309364139bbf1cce0cf598f89e246efda109
josemp10 <josemp10@gmail.com>
2016-02-15 21:12:17 +0100

    missionservice

--- modules/Mission/missionController.js
+++ modules/Mission/missionController.js
@@ -16,24 +16,16 @@
    You should have received a copy of the GNU General Public License
    along with Project Configuration.  If not, see <http://www.gnu.org/licenses/>. */
 
-appConfigProjectMSF.controller('missionController', ["$scope", '$filter', "commonvariable", "OrgUnit", "OrgUnitGroupsOrgUnit", "DataSets", "OrgUnitChildren", "FilterResource", "DataSetsOrgUnit", "$modal", "User", "validatorService", function ($scope, $filter, commonvariable, OrgUnit, OrgUnitGroupsOrgUnit, DataSets, OrgUnitChildren, FilterResource, DataSetsOrgUnit, $modal, User, validatorService) {
+appConfigProjectMSF.controller('missionController', ["$scope", '$filter', "commonvariable", "$modal", "validatorService", "missionService", "OrgUnitChildren",
+                                                     function ($scope, $filter, commonvariable, $modal, validatorService, missionService, OrgUnitChildren) {
     var $translate = $filter('translate');
     //Load Data of OU selected
     $scope.prevOu = "";
     $scope.showbutton = true;
     $scope.CreateDatasetVaccination = true;
-
-    $scope.initValue = function () {
-        ///OrgunitGroupSet 
-        $scope.projectTypeId = commonvariable.ouGroupsetId.ProjectType;
-        $scope.populationTypeId = commonvariable.ouGroupsetId.PopulationType;
-        $scope.typeManagementId = commonvariable.ouGroupsetId.TypeManagement;
-        $scope.gsEventId = commonvariable.ouGroupsetId.Event;
-        $scope.gsContextId = commonvariable.ouGroupsetId.Context;
-    }
-    $scope.initValue();
+    
+    missionService.initValue($scope);
    
-
     // if there exist Vaccination DataSet   then show the DataElements  
     $scope.showDataSetforEdit = function () {
         $scope.showForm(2);
@@ -43,53 +35,12 @@ appConfigProjectMSF.controller('missionController', ["$scope", '$filter', "commo
        $scope.messages.splice(index, 1);
   	};
 	
-	$scope.messages=[];
-	
-	
+	$scope.messages=[];	
 	
 	$scope.showfields=false;
-	
-	
-	$scope.saveusers=function(){
-		
-		var user
-		
-		for (var i=0; i<=10;i++) {
-			
-			user={}
-			user.surname = commonvariable.userDirective
-			user.userCredentials= {}
-			user.userCredentials.password=commonvariable.users.passwd
-			user.organisationUnits = [{"id":commonvariable.NewOrganisationUnit.id}]
-			user.dataViewOrganisationUnits = [{"id":commonvariable.NewOrganisationUnit.id}]
-			user.userGroups = [{"id":commonvariable.users.uid_project_users_userGroup}]
-			
-			if (i == 0) { //MFP User
-				user.firstName = commonvariable.users.postfix_mfp
-				user.userCredentials.userRoles = [{"id":commonvariable.users.uid_role_mfp}]
-				user.userCredentials.username=commonvariable.users.prefix + "-" + commonvariable.userDirective + "-" + commonvariable.users.postfix_mfp				
-			} else { //FIELD User
-				user.firstName = commonvariable.users.postfix_fielduser + i
-				user.userCredentials.userRoles = [{"id":commonvariable.users.uid_role_fielduser}]
-				user.userCredentials.username=commonvariable.users.prefix + "-" + commonvariable.userDirective + "-" + commonvariable.users.postfix_fielduser + i				
-			}
-			
-			console.log(user)
-			
-			User.POST(user).$promise.then(function (data) {
-				
-				console.log(data)
-				
-			});
-			
-		}
-		
-		
-	}
 
 	///Save project
 	$scope.projectsave=function(){
-
 
 		var newOu={//payload
 				name:commonvariable.ouDirective,
@@ -99,68 +50,29 @@ appConfigProjectMSF.controller('missionController', ["$scope", '$filter', "commo
 	            openingDate: $filter('date')($scope.propendate,'yyyy-MM-dd'),
 	           	parent: commonvariable.OrganisationUnitParentConf
 				};
-	    ///validate if object is ok.
+
+		///validate if object is ok.
 		validatorService.emptyValue(newOu).then(function (result) {
-		    if (result == false)
-		    {
-
-		OrgUnit.POST({},newOu)
-		.$promise.then(function(data){
-    		  console.log(data);
-    		 // if (data.response.status == "SUCCESS") { ///verificar que en la versi�n 2.19 y 2.20 sea data.response.status
-    		  if (data.response.importCount.imported >= 1) {
-    		      commonvariable.RefreshTreeOU=true;
-				  newOu.id=data.response.lastImported;
-				  commonvariable.NewOrganisationUnit=newOu;
-				 
-				  if (commonvariable.orgUnitGroupSet[commonvariable.ouGroupsetId.ProjectType] != undefined)
-				      OrgUnitGroupsOrgUnit.POST({uidgroup:commonvariable.orgUnitGroupSet[commonvariable.ouGroupsetId.ProjectType].id, uidorgunit:newOu.id});
-				  if (commonvariable.orgUnitGroupSet[commonvariable.ouGroupsetId.PopulationType] != undefined)
-				      OrgUnitGroupsOrgUnit.POST({ uidgroup: commonvariable.orgUnitGroupSet[commonvariable.ouGroupsetId.PopulationType].id, uidorgunit: newOu.id });
-				  if (commonvariable.orgUnitGroupSet[commonvariable.ouGroupsetId.TypeManagement] != undefined)
-				      OrgUnitGroupsOrgUnit.POST({ uidgroup: commonvariable.orgUnitGroupSet[commonvariable.ouGroupsetId.TypeManagement].id, uidorgunit: newOu.id });
-				  if (commonvariable.orgUnitGroupSet[commonvariable.ouGroupsetId.Event] != undefined)
-				      OrgUnitGroupsOrgUnit.POST({ uidgroup: commonvariable.orgUnitGroupSet[commonvariable.ouGroupsetId.Event].id, uidorgunit: newOu.id });
-				  if (commonvariable.orgUnitGroupSet[commonvariable.ouGroupsetId.Context] != undefined)
-				      OrgUnitGroupsOrgUnit.POST({ uidgroup: commonvariable.orgUnitGroupSet[commonvariable.ouGroupsetId.Context].id, uidorgunit: newOu.id });
-				  
-
-				  FilterResource.GET({resource:'dataSets', filter:'code:eq:'+"DS_VST_3"}).$promise
-				  		.then(function(response){
-				  			
-				  			if (response.dataSets.length>0) {
-				  				
-				  				var dataSet = response.dataSets[0];
-				  				DataSetsOrgUnit.POST({uidorgunit:newOu.id, uiddataset:dataSet.id});
-				  			}
-				  							  			
-				  		});
-				  				  
-				  
-				  $scope.saveusers();
-				  
-				 //set message variable
-				  $scope.messages.push({ type: "success", text: $translate('PROJECT_SAVED') });
-				  $scope.hideForm();
-				//clear txtbox
-				$scope.projectName="";
-
-			}
-			else{
-    		      $scope.messages.push({ type: "danger", text: $translate('PROJECT_NOSAVED') });
-			}
-		});
+		    if (result == false) {
+		    	
+		    	missionService.saveProject(newOu).then(function(imported){
+		    		if (imported) {
+		    		      commonvariable.RefreshTreeOU=true;
+		    		      missionService.saveUsers();
+		 				 //set message variable
+						  $scope.messages.push({ type: "success", text: $translate('PROJECT_SAVED') });
+						  $scope.hideForm();
+						  //clear txtbox
+						  $scope.projectName="";
+		    		} else 		        
+		   		      	$scope.messages.push({ type: "danger", text: $translate('PROJECT_NOSAVED') });
+		    	});
+		    	
 		    }
-		    else {
-		        $scope.messages.push({
-		            type: "warning",
-		            text: $translate("FORM_MSG_EMPTYFIELD")
-		        });
-		    }
-
-
-		});
-
+		    else 
+		    	$scope.messages.push({type: "warning", text: $translate("FORM_MSG_EMPTYFIELD")});
+				    
+		 });
 				
 	};
 	
@@ -256,67 +168,7 @@ appConfigProjectMSF.controller('missionController', ["$scope", '$filter', "commo
 	           };
 	       }
 	   });
-    
-    ////////////////////////For Edit //////////////////////////////////////////
-
-    ///enable textBox
-	   $scope.operation = 'show';
-	   $scope.enableforEdit = function () {
-	       $scope.operation = 'edit';
-	       commonvariable.NewOrganisationUnit = [];
-	   }
-	   $scope.enableforshow = function () {
-	       $scope.operation = 'show';
-	   }
-
-    ////Edit mission
-	   $scope.EditMission = function () {
-		   
-	       var newOu = {//payload
-	           name: commonvariable.ouDirective,
-	           level: commonvariable.OrganisationUnit.level,
-	           shortName: commonvariable.ouDirective,
-	           openingDate: $scope.mdopendate,
-	           parent: commonvariable.OrganisationUnitParentConf
-	       };
-
-	       ///
-	       $scope.messages.push({ type: "success", text: $translate('MISSION_UPDATED') });
-
-
-	   }
-
-
-
-   /////modal for delete message
-
-	   $scope.modalDelete = function (size) {
-
-	       var modalInstance = $modal.open({
-	           templateUrl: 'ModalConfirm.html',
-	           controller: 'ModalConfirmCtrl',
-	           size: size,
-	           resolve: {
-	               information: function () {
-	                   return { tittle: $translate('MISSION_DELETE_TITTLE'), description: $translate('MISSION_DELETE_DESCRIPTION'), id: commonvariable.OrganisationUnit.id };
-	               }
-	           }
-	       });
-
-	       modalInstance.result.then(function (option) {
-	           if (option == true) {
-	               $scope.messages.push({ type: "success", text: $translate('MISSION_DELETED') });
-	           }
-	           else {
-	               $scope.messages.push({ type: "error", text: $translate('MISSION_NODELETED') });
-	           }
-	       }, function () {
-	           console.log('Modal dismissed at: ' + new Date());
-	       });
-	   };
-
-	
-  
+      
 }]);
 
 
